a9462ff79065ea7a1391fc57a35c8cfab8be3d95
Stefano.In91 <ste.ince91@gmail.com>
2022-10-28 17:13:29 +0200

    adds palindrome function

--- palindroma/js/app.js
+++ palindroma/js/app.js
@@ -1,2 +1,28 @@
 "use strict";
 
+// Chiedere all’utente di inserire una parola
+
+const userWord = prompt("Inserisci una parola/frase");
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindrome(string) {
+   let wordCounter = 0;
+   for (let i = 0; i < string.length; i++) {
+      console.log(string[i], string[string.length - 1 - i])
+      if (string[i] === string[string.length - 1 - i]){
+         wordCounter++;
+         console.log(wordCounter);
+      }
+   }
+   if (wordCounter === string.length){
+      return true;
+   } else {
+      return false;
+   }
+}
+
+if (palindrome(userWord)) {
+   alert("La parola inserita è palindroma");
+} else {
+   alert("La parola non è palindroma");
+}
