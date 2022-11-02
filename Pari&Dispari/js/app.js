c924cad354cbefde10319dca92aa71541fc29066
Stefano.In91 <ste.ince91@gmail.com>
2022-11-02 10:54:12 +0100

    adds while variant to palindrome, tidies up

--- Pari&Dispari/js/app.js
+++ Pari&Dispari/js/app.js
@@ -3,30 +3,35 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 let userChoice;
 do {
-   userChoice = prompt("Pari o Dispari?").toLowerCase();
-} while ( userChoice !== "pari" && userChoice !== "dispari" )
+  userChoice = prompt("Pari o Dispari?").toLowerCase();
+} while (userChoice !== "pari" && userChoice !== "dispari");
 
 let userNumber;
 do {
-   userNumber = Number(prompt("Scegli un numero da 1 a 5"));
-} while ( userNumber <= 0 || userNumber > 5 )
+  userNumber = Number(prompt("Scegli un numero da 1 a 5"));
+} while (userNumber <= 0 || userNumber > 5);
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function getRandomIntInclusive(min, max) {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   return Math.floor(Math.random() * (max - min + 1) + min);
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-const pcNumber = getRandomIntInclusive(1, 5);
+const pcNumber = getRndInteger(1, 5);
 // Sommiamo i due numeri
 const sum = userNumber + pcNumber;
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function isEven(number) {
-   return number % 2 === 0;
+  return number % 2 === 0;
 }
 // Dichiariamo chi ha vinto.
-if ( (userChoice === "pari" && isEven(sum)) || (userChoice === "dispari" && !isEven(sum)) ) {
-   alert(`La somma del tuo numero e quello del pc è ${sum}, avendo scelto ${userChoice}, hai Vinto`);
+if (
+  (userChoice === "pari" && isEven(sum)) ||
+  (userChoice === "dispari" && !isEven(sum))
+) {
+  alert(
+    `La somma del tuo numero e quello del pc è ${sum}, avendo scelto ${userChoice}, hai Vinto`
+  );
 } else {
-   alert(`La somma del tuo numero e quello del pc è ${sum}, avendo scelto ${userChoice}, hai Perso`);
+  alert(
+    `La somma del tuo numero e quello del pc è ${sum}, avendo scelto ${userChoice}, hai Perso`
+  );
 }
--- palindroma/js/app.js
+++ palindroma/js/app.js
@@ -5,22 +5,38 @@ const userWord = prompt("Inserisci una parola/frase").toLowerCase();
 
 // Creare una funzione per capire se la parola inserita è palindroma
 function palindrome(string) {
-   let equalLetterCounter = 0;
-   for (let i = 0; i < string.length; i++) {
-      // Comparazione prima lettera con ultima, seconda con penultima e via scalando
-      console.log(string[i], string[string.length - 1 - i])
-      if (string[i] === string[string.length - 1 - i]){
-         // Incremento contatore per ogni lettera uguale a quella nella stessa posizione a partire dal fondo
-         equalLetterCounter++;
-         console.log(equalLetterCounter);
-      }
-   }
-   // Restituisco risultato Booleano su eguaglianza
-   return equalLetterCounter === string.length;
+  let equalLetterCounter = 0;
+  for (let i = 0; i < string.length; i++) {
+    // Comparazione prima lettera con ultima, seconda con penultima ecc
+    if (string[i] === string[string.length - 1 - i]) {
+      equalLetterCounter++;
+    }
+  }
+  // Restituisco risultato Booleano su eguaglianza
+  return equalLetterCounter === string.length;
 }
 // Risultato comparazione
 if (palindrome(userWord)) {
-   alert("La parola/frase è palindroma");
+  alert("La parola/frase è palindroma");
 } else {
-   alert("La parola/frase non è palindroma");
+  alert("La parola/frase non è palindroma");
+}
+
+// Variante con ciclo while
+function isPalindrome(string) {
+  console.log("variante while");
+  let counter = 0;
+  do {
+    if (string[counter] === string[string.length - 1 - counter]) {
+      counter++;
+    } else {
+      return false;
+    }
+  } while (counter < string.length);
+  return true;
+}
+if (isPalindrome(userWord)) {
+  alert("La parola/frase è palindroma");
+} else {
+  alert("La parola/frase non è palindroma");
 }
